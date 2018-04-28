df144a1484db06a1373c1a6e1435b18b741ca67c
Unknown <eyals.code@gmail.com>
2018-04-28 17:55:29 +0300

    Question 6

--- q6.js
+++ q6.js
@@ -1,7 +1,41 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var L1_ast_1 = require("./L1-ast");
+var L1_ast_2 = require("./L1-ast");
 // Implement the following function:
 exports.unparse = function (x) {
-    return "Implement me!";
+    return L1_ast_1.isProgram(x) ? unparseProgram(x) :
+        L1_ast_1.isDefineExp(x) ? unparseDefineExp(x) :
+            L1_ast_1.isCExp(x) ? unparseCExp(x) :
+                '';
 };
+var unparseProgram = function (x) {
+    return x.exps.map(function (exp) { return exports.unparse(exp); })
+        .reduce(function (acc, curr) { return acc + curr; });
+};
+var unparseDefineExp = function (x) {
+    return '( define ' + x.var.var + exports.unparse(x.val) + ')';
+};
+var unparseCExp = function (x) {
+    return L1_ast_1.isNumExp(x) ? x.val.toString() :
+        L1_ast_1.isBoolExp(x) ? unparseBoolean(x) :
+            L1_ast_1.isPrimOp(x) ? x.op :
+                L1_ast_1.isVarRef(x) ? x.var :
+                    L1_ast_1.isVarDecl(x) ? '(let ' + x.var + ')' :
+                        L1_ast_1.isAppExp(x) ? unparseApp(x) :
+                            '';
+};
+var unparseBoolean = function (x) {
+    return x.val ? '#t' : '#f';
+};
+var unparseApp = function (x) {
+    var rands = x.rands.map(function (rand) { return exports.unparse(rand); });
+    return '(' + unparseCExp(x.rator) +
+        rands.reduce(function (acc, curr) { return acc + ' ' + curr; }, '') + ')';
+};
+console.log(exports.unparse(L1_ast_2.parseL1("(L1 " +
+    "(define x 5)" +
+    "(+ x 5)" +
+    "(+ (+ (- x y) 3) 4)" +
+    "(and #t x))")));
 //# sourceMappingURL=q6.js.map
--- q6.ts
+++ q6.ts
@@ -5,8 +5,48 @@ import { parseL1 } from "./L1-ast";
 import { first, isEmpty, rest } from "./L1-ast";
 import * as assert from "assert";
 import { filter, map, reduce } from "ramda";
+import { BoolExp, AppExp } from "./L3-ast";
 
 // Implement the following function:
 export const unparse = (x: Program | DefineExp | CExp) : string | Error => {
-    return "Implement me!";
+    return isProgram(x) ? unparseProgram(x) : 
+    isDefineExp(x) ? unparseDefineExp(x) :
+    isCExp(x) ? unparseCExp(x) :
+    ''
 }
+
+const unparseProgram = (x : Program) : string | Error => {
+    return x.exps.map((exp) => unparse(exp))
+        .reduce((acc : string, curr : string) => acc + curr)
+}
+
+const unparseDefineExp = (x: DefineExp) : string | Error => {
+    return '( define ' + x.var.var + unparse(x.val) + ')'
+}
+
+const unparseCExp = (x : CExp) : string | Error => {
+    return isNumExp(x)? x.val.toString() :
+    isBoolExp(x)? unparseBoolean(x) :
+    isPrimOp(x)? x.op :
+    isVarRef(x)? x.var :
+    isVarDecl(x)? '(let ' + x.var + ')':
+    isAppExp(x)? unparseApp(x as AppExp):
+    ''
+}
+
+const unparseBoolean = (x : BoolExp) : string => {
+    return x.val? '#t' : '#f'
+}
+
+const unparseApp = (x: AppExp) => {
+    let rands = x.rands.map((rand) => unparse(rand as DefineExp | CExp))
+
+    return '(' + unparseCExp(x.rator as CExp) + 
+        rands.reduce((acc : string, curr) => acc + ' ' + curr, '') + ')'
+}
+
+console.log(unparse(parseL1("(L1 " + 
+    "(define x 5)"+
+    "(+ x 5)"+
+    "(+ (+ (- x y) 3) 4)"+
+    "(and #t x))"))); 
